1d9fd8338d3e4795db879b05c93e117d34bc5828
Khushbu Khushbu <khushbu0@sheridancollege.ca>
2026-01-28 15:44:53 -0500

    Add addMessage feature

---
+++ ICE4/src/main/java/com/mycompany/ice4/ICE4.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ice4;
+
+/**
+ *
+ * @author khush
+ */
+public class ICE4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public String addMessage() {
+    return "Feature addMessage implemented";
+}
+
+}
